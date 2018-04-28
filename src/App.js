314926801d24d77c5c798965078dc02a090a53bc
John Arthur <thorjarhun@gmail.com>
2018-04-27 19:38:40 -0500

    add flow

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// @flow
 import React, { Component } from 'react';
 import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// @flow
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 // Put any other imports below so that CSS from your
